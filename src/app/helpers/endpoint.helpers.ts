337ed4bd9588f6708046064f8a5e9aa8c576440c
arturtembe <malunganeartur@gmail.com>
2024-05-24 16:04:58 +0200

    update01

---
+++ src/app/helpers/endpoint.helpers.ts
@@ -0,0 +1,9 @@
+
+const host:string = `http://localhost:3333/shopee/mongodb`;
+
+const endpoint = {
+    register: `${host}/register`,
+    login: `${host}/login`,
+}
+
+export default endpoint;
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -5,14 +5,15 @@ import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 import { map, Observable } from 'rxjs';
 import { Usuario } from '../../interfaces/Usuario';
+import endpoint from '../../helpers/endpoint.helpers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  private apiUrl_LOGIN='http://localhost:3000/db/controllers/shoopee/usuarioController/loginUser.php';
-  private apiUrl_REGISTER='http://localhost:3000/db/controllers/shoopee/usuarioController/registerUser.php';
+  //private apiUrl_LOGIN='http://localhost:3000/db/controllers/shoopee/usuarioController/loginUser.php';
+  //private apiUrl_REGISTER='http://localhost:3000/db/controllers/shoopee/usuarioController/registerUser.php';
   
   private apiUrl_TOKEN='http://localhost:3000/db/controllers/shoopee/usuarioController/getUserAPI.php';
 
@@ -20,12 +21,12 @@ export class UsuarioService {
 
   loginUsuario(usuario:any):Observable<Usuario[]>{
 
-    return this.http.post<Usuario[]>(this.apiUrl_LOGIN,usuario)
+    return this.http.post<Usuario[]>(endpoint.login,usuario)
   }
 
   registerUsuario(usuario:any):Observable<Usuario[]>{
 
-    return this.http.post<Usuario[]>(this.apiUrl_REGISTER,usuario)
+    return this.http.post<Usuario[]>(endpoint.register,usuario)
   }
 
   tokenUsuario(token:string|null):Observable<Usuario[]>{
